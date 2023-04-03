76a0e00c695c53e4a07a39e5da6e272df4e3ca27
shikarisohan <moksedur.rahman.sohan@gmail.com>
2023-04-03 18:58:46 +0600

    done

---
+++ expressApp/routes/wiki.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", function (req, res, next) {
+  res.send("wiki home page");
+});
+router.get("/about", function (req, res, next) {
+  res.send("About this wiki");
+});
+
+router.get("/students", function (req, res, next) {
+  res.send(
+    "List of students :\n 1.Sohan (2018331068) \n 2. Owishi (2018331022)\n"
+  );
+});
+router.get("/time", function (req, res, next) {
+  const now = new Date();
+  const day = now.toLocaleString("en-US", { day: "2-digit" });
+  const month = now.toLocaleString("en-US", { month: "2-digit" });
+  const year = now.getFullYear();
+
+  const formattedDate = `${day}/${month}/${year}`;
+
+  res.send("Current date is : " + formattedDate);
+});
+module.exports = router;
