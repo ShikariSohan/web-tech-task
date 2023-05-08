14a99ef5cef35130f9b5a31af90d57dc14545393
shikarisohan <moksedur.rahman.sohan@gmail.com>
2023-05-08 10:10:30 +0600

    manual added

--- expressApp/app.js
+++ expressApp/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/wiki", wikiRouter);
+app.use("/", wikiRouter);
 
 
 
--- expressApp/routes/wiki.js
+++ expressApp/routes/wiki.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", function (req, res, next) {
+router.get("/wiki", function (req, res, next) {
   res.send("wiki home page");
 });
 router.get("/about", function (req, res, next) {
